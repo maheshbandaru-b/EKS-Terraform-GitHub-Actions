311e481d3b61a2491c193b1c6e945716463a75f9
maheshbandaru-b <79903684+maheshbandaru-b@users.noreply.github.com>
2024-10-20 11:29:04 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "prod2024"
+    bucket         = "maheshgoud20241020"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
